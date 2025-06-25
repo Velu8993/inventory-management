287ad306b5d5adc0ab3ecdd161848a72b7b190c2
Kanagavell R <kanagavel8993@gmail.com>
2025-06-25 15:57:03 +0530

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// ROUTE IMPORTS
+import dashboardRoutes from "./routes/dashboardRoutes"; // http://localhost:8000/dashboard
+import productRoutes from "./routes/productRoutes"; // http://localhost:8000/products
+import userRoutes from "./routes/userRoutes"; // http://localhost:8000/users
+import expenseRoutes from "./routes/expenseRoutes"; // http://localhost:8000/expenses
+
+// CONFIGURATIONS
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROOUTES
+app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
+
+// SERVER
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
