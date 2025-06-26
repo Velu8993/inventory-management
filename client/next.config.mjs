38626fa52cad5543b9624fed857bed4dc6defedf
Kanagavell R <kanagavel8993@gmail.com>
2025-06-26 15:20:57 +0530

    added images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement-velu.s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
